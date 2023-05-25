7626a20be45c2543ef42e51c628d30ca038140ad
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-05-25 20:22:17 +0200

    added a template folder

--- ecommerce/app/models.py
+++ ecommerce/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Models):
+    name = models.CharField(max_length=50)
+    
+    @staticmethod
+    def get_all_categories():
+        return Category.objects.all()
+    
+    def __str__(self):
+        return self.name
+    
--- ecommerce/app/urls.py
+++ ecommerce/app/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
-from . import setting 
+from . import settings
 
 urlpatterns = [
-    path('admin/',admin.site.urls)
+    path('admin/',admin.site.urls),
     path('',include('app.urls'))
-]+ static(setting.MEDIA_URL,document_root=setting.MEDIA_ROOT)
+ ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
